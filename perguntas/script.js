9cd1f7bed42518baecf721e1915cd79b4f321033
Adrian Gobara <adriangobara@gmail.com>
2024-07-30 23:41:46 -0300

    questão fodaaaa

---
+++ perguntas/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.parede').forEach(function(parede) {
+    parede.addEventListener('mouseover', function() {
+        const url = this.getAttribute('data-url');
+        window.location.href = url;
+    });
+});
+
+document.querySelector('.meta').addEventListener('mouseover', function() {
+    const url = this.getAttribute('data-url');
+    window.location.href = url;
+});
